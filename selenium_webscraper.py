d4ef7d683104d58b2d6ff1cf3b5c0a80bb515c90
Elly Kuhlman <elly.kuhlman@gmail.com>
2017-10-18 15:08:45 -0400

    added bs4 parsing

--- selenium_webscraper.py
+++ selenium_webscraper.py
@@ -1,3 +1,4 @@
+import bs4
 from selenium import webdriver
 
 path_to_webdriver = '/Users/ellykuhlman/Downloads/chromedriver'
@@ -9,3 +10,18 @@ browser.get(url)
 html = browser.page_source
 
 print(html)
+
+soup = bs4.BeautifulSoup(html)
+
+
+
+
+
+recipe_soup = soup.find_all('div', class_='style-iwqinworimageItemzoom')
+
+recipes = {}
+
+for recipe in recipe_soup:
+    name = recipe.find(itemprop='name').contents[0]
+    ingredient_soup = recipe.find(itemprop="description").find_all("span")
+    recipes[name] = [i.contents[0] for i in ingredient_soup if i.contents]
